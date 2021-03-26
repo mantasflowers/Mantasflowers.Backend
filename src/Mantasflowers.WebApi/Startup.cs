0d26f1ed7d03fe5d91e66d80681d8a0a81501f26
Dominykas Prievelis <edomasss@gmail.com>
2021-03-26 17:49:12 +0200

    adding autofac

--- src/Mantasflowers.WebApi/Startup.cs
+++ src/Mantasflowers.WebApi/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Mantasflowers.WebApi
@@ -32,6 +26,11 @@ namespace Mantasflowers.WebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mantasflowers.WebApi", Version = "v1" });
             });
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
