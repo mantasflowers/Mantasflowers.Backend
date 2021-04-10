bdc83bd1e3d1063ca00bba9cbcf3f926b38899cb
Dominykas Prievelis <edomasss@gmail.com>
2021-04-10 03:25:26 +0300

    move error handling after db initialization

--- src/Mantasflowers.WebApi/Startup.cs
+++ src/Mantasflowers.WebApi/Startup.cs
@@ -75,9 +75,9 @@ namespace Mantasflowers.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
-
             app.EnsureDatabaseState();
+
+            app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
 
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mantasflowers API v1"));
